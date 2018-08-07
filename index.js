38fdd66883892354438d0ffd563e5b72a0b93f0f
clairemacdonald <claire.macdonald@blis.com>
2018-08-07 17:22:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* Enter the code to remove the main node element under this comment */
+element.remove(main) /* Enter the code to remove the main node element under this comment */
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
