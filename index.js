fc3306c41efb34e48681c577a1f485f40ece61d4
clairemacdonald <claire.macdonald@blis.com>
2018-08-07 17:24:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 element.remove(main) /* Enter the code to remove the main node element under this comment */
 
 element.innerHTML = 'Hello, DOM!'
-element.style.newHeader = 'red'/* Create your new element here and assign it to newHeader */
+element.style.newHeader = 'red';
+/* Create your new element here and assign it to newHeader */
 const newHeader = null;
